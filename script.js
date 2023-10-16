8da598125edc9f096cf66d2f7b78d7ac75bac7e6
Pranshu-15 <pranshu923@gmail.com>
2023-10-16 06:26:32 +0530

    final-cut

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-let currentDisplay = '';
-document.querySelector('#displayBox').value = currentDisplay;
+let current_display = '';
+document.querySelector('#display').value = current_display;
+
+let append_value = (value) => {
+    current_display += value;
+    document.querySelector('#display').value = current_display;
+}
+
+let result = () => {
+    current_display = eval(current_display);
+    document.querySelector('#display').value = current_display
+}
+
+let append_mod = () => {
+    let a = current_display.slice(0, -1);
+    console.log(a);
+}
+
+let delete_last = () => {
+    current_display = current_display.slice(0, -1);
+    document.querySelector('#display').value = current_display;
+}
+
+let clear_all = () => {
+    current_display = '';
+    document.querySelector('#display').value = current_display;
+} 
